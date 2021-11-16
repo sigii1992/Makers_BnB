c496f3a32c7d81a74d2080f820fc771b6e387707
Sigita <sigita.zeb92@gmail.com>
2021-11-16 14:49:32 +0000

    Implement test for method All

--- spec/properties_spec.rb
+++ spec/properties_spec.rb
@@ -1,1 +1,15 @@
 require 'property'
+
+describe '.all' do
+  it 'returns a list of all properties' do
+    connection = PG.connect(dbname: 'makersbnb_test')
+    #add the test data
+    Property.create(name: 'Buckingham Palace', description: "Big old house, one old lady currently inhabiting", price_per_night: 10)
+    properties = Property.all
+
+    expect(properties[0].name).to eq 'Buckingham Palace'
+    expect(properties[0].description).to eq "Big old house, one old lady currently inhabiting"
+    expect(properties[0].price_per_night.to_i).to eq 10
+  end
+
+end
