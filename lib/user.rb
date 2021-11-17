1a91936923708fe0873a90b3bf9ee8e8dbfd1af6
Sigita <sigita.zeb92@gmail.com>
2021-11-17 17:43:20 +0000

    Change method name

--- lib/user.rb
+++ lib/user.rb
@@ -21,7 +21,7 @@ attr_reader :id, :name, :email, :password
     User.new(id: result[0]['id'], name: result[0]['name'], email: result[0]['email'], password: result[0]['password'])
   end
   
-  def self.find(email:)
+  def self.find_id(email:)
     if ENV['ENVIRONMENT'] == 'test'
       connection = PG.connect(dbname: 'makersbnb_test')
     else
