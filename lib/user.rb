ca6031a8034714eadbf47b7d84ea46c270cc5999
Monaliza <mo.o@hotmail.co.uk>
2021-11-18 12:33:56 +0000

    log in work in progress

--- lib/user.rb
+++ lib/user.rb
@@ -20,7 +20,8 @@ attr_reader :id, :name, :email, :password
     result = connection.exec_params("INSERT INTO users (name, email, password) VALUES($1, $2, $3) RETURNING id, name, email, password;", [name, email, password])
     User.new(id: result[0]['id'], name: result[0]['name'], email: result[0]['email'], password: result[0]['password'])
   end
-  
+
+
   def self.find_id(email:)
     if ENV['ENVIRONMENT'] == 'test'
       connection = PG.connect(dbname: 'makersbnb_test')
@@ -30,7 +31,33 @@ attr_reader :id, :name, :email, :password
 
     result = connection.exec_params("SELECT * FROM users WHERE email = $1", [email])
     result[0]['id']
-    # User.new(id: result[0]['id'], name: result[0]['name'], email: result[0]['email'], password: result[0]['password'])
+    
   end
+
+
+  def self.log_in(email:, password:)
+    if ENV['ENVIRONMENT'] == "test"
+       connection = PG.connect(dbname: 'makersbnb_test')
+    else
+      connection = PG.connect(dbname: 'makersbnb')
+    end
+
+    result = connection.exec("SELECT * FROM users WHERE email = $1", [email])
+    return unless result.any?
+    return unless result[0]['password'] == password
+    User.new (email: result[0]['email'], name: result[0]['name'])
+  end
+
+
+        # def log_in(email:, password:)
+        #   if user.email == false
+        #     "User does not exist" 
+        #   else
+        #    if user.password == password
+        #     "Welcome to the page"
+        #    else 
+        #   end
+        # end
+
 
 end
--- spec/users_spec.rb
+++ spec/users_spec.rb
@@ -11,15 +11,41 @@ describe '.create' do
   end
 end
 
-describe '.find' do
-  it 'finds a user by email' do
-    connection = PG.connect(dbname: 'makersbnb_test')
-    user = User.create(name: 'Hagrid', email: 'hagrid@gmail.com', password: 'hagrid123')
-    result = User.find(email: user.email)
 
+describe '.find_id' do
+  it 'finds a user by id' do
+    user = User.create(name: 'Hagrid', email: 'hagrid@gmail.com', password: 'hagrid123')
+    result = User.find_id(email: user.email)
     expect(result).to eq user.id
-    # expect(result.name).to eq user.name
-    # expect(result.email).to eq user.email
-    # expect(result.password).to eq user.password
   end
 end
+
+# describe '.get_id' do
+#   it 'returns the id of the logged-in user' do
+#     user = Account.create(name: "Bigmoneybob", email: "Bigmoneybob@hotmail.com", password: "test")
+#     user_id = Account.get_id(email: user.email)
+#     expect(user_id).to be_a(String)
+#   end
+# end
+# end
+
+describe '.log_in' do 
+  it 'logs in a user' do
+    User.create(name: 'Hagrid', email: 'hagrid@gmail.com', password: 'hagrid123')
+    user = User.log_in(email: 'hagrid@gmail.com', password: 'hagrid123')
+    expect(user.name).to eq 'Hagrid'
+  end
+end
+
+
+
+
+# describe '.find' do
+#   it 'finds a user by ID' do
+#     user = User.create(email: 'test@example.com', password: 'password123')
+#     result = User.find(id: user)
+
+#     expect(result.id).to eq user.id
+#     expect(result.email).to eq user.email
+#   end
+
